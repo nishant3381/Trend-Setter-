d7af5c6647d42f1a8a7fe9e0f5e66d2051ef4f4a
nishant3381 <143287040+nishant3381@users.noreply.github.com>
2024-07-22 16:02:11 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,34 @@
+import p1_img from './product_1.png'
+import p2_img from './product_2.png'
+import p3_img from './product_3.png'
+import p4_img from './product_4.png'
+
+let data_product = [
+  {
+    id:1,
+    name:"CLUSE Pink watch for women",
+    image:p1_img,
+    new_price:500.00,
+    old_price:800.50,
+  },
+  {id:2,
+    name:"RAGA Pearls watch",
+    image:p2_img,
+    new_price:850.00,
+    old_price:1200.50,
+  },
+  {id:3,
+    name:"ROLEX diamond watch",
+    image:p3_img,
+    new_price:6000.00,
+    old_price:10000.50,
+  },
+  {id:4,
+    name:"TIMEX silver braclet watch",
+    image:p4_img,
+    new_price:1000.00,
+    old_price:1500.00,
+  },
+];
+
+export default data_product;
---
+++ new_collections.js
@@ -0,0 +1,69 @@
+import p1_img from "./product_12.png";
+import p2_img from "./product_35.png";
+import p3_img from "./product_14.png";
+import p4_img from "./product_8.png";
+import p5_img from "./product_15.png";
+import p6_img from "./product_2.png";
+import p7_img from "./product_17.png";
+import p8_img from "./product_28.png";
+
+let new_collections = [
+  {
+    id: 12,
+    name: "ROLEX Diamond watch",
+    image: p1_img,
+    new_price: 5000.0,
+    old_price: 8000.0,
+  },
+  {
+    id: 35,
+    name: "LONGINES 5-Star watch",
+    image: p2_img,
+    new_price: 850.0,
+    old_price: 1200.0,
+  },
+  {
+    id: 14,
+    name: "G-SHOCK CASIO advanced watch",
+    image: p3_img,
+    new_price: 6000.0,
+    old_price: 10000.,
+  },
+  {
+    id: 8,
+    name: "ROLEX silver watch",
+    image: p4_img,
+    new_price: 1000.0,
+    old_price: 1500.0,
+  },
+  {
+    id: 15,
+    name: "VERSACE Gold watch",
+    image: p5_img,
+    new_price: 5000.0,
+    old_price: 8000.0,
+  },
+  {
+    id: 2,
+    name: "RAGA pearls watch",
+    image: p6_img,
+    new_price: 850.0,
+    old_price: 1200.0,
+  },
+  {
+    id: 17,
+    name: "CASIO RED-BULL racing edition",
+    image: p7_img,
+    new_price: 6000.0,
+    old_price: 10000.0,
+  },
+  {
+    id: 28,
+    name: "G-SHOCK CASIO watch",
+    image: p8_img,
+    new_price: 1000.0,
+    old_price: 1500.0,
+  },
+];
+
+export default new_collections;
